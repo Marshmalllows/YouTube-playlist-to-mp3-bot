c3c5716f52b6ae56bc7eb949e4e85e8a63bcb035
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2024-08-03 16:03:59 +0300

    Add bot URL handle in MessageHandler

--- YouTube playlist to mp3 bot/Program.cs
+++ YouTube playlist to mp3 bot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using YoutubeExplode;
+using YoutubeExplode.Exceptions;
 using File = System.IO.File;
 
 namespace YouTube_playlist_to_mp3_bot;
@@ -17,7 +20,53 @@ internal class Program
 
     private static async void MessageHandler(ITelegramBotClient client, Update update)
     {
-        //Some code
-        await Task.CompletedTask;
+        var reserveChatId = long.Parse(File.ReadAllText(@"..\..\..\reserveChatId.txt"));
+        var chatId = update.Message?.Chat.Id ?? reserveChatId;
+        var message = update.Message;
+        var youtube = new YoutubeClient();
+
+        if (message?.Text is null)
+        {
+            await client.SendTextMessageAsync(chatId, "The message must be either a text or a link");
+            return;
+        }
+
+        if (message.Text == "/start")
+        {
+            await client.SendTextMessageAsync(chatId, "Hello!\n" +
+                                                      "I am a bot for downloading MP3 files from all videos in your playlist.\n" +
+                                                      "To start the download, please send me the link to the playlist you want to " +
+                                                      "convert to audio format.");
+        }
+        else
+        {
+            try
+            {
+                var playlist = await youtube.Playlists.GetAsync(message.Text);
+                var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Yes", "Yes"),
+                        InlineKeyboardButton.WithCallbackData("No", "No")
+                    }
+                });
+                await client.SendTextMessageAsync(chatId, $"Are you sure you want to download audio from " + 
+                                                          $"{playlist.Count} videos from \"{playlist.Title}\" playlist?", 
+                    replyMarkup: inlineKeyboard);
+            }
+            catch (PlaylistUnavailableException)
+            {
+                await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
+            }
+            catch (ArgumentException)
+            {
+                await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
     }
 }
