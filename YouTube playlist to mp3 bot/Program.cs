69a97d43959af5d7557b8370eb850db9dc3ad442
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2024-08-03 17:13:13 +0300

    Refactor code and add inline keyboard handle

--- YouTube playlist to mp3 bot/Program.cs
+++ YouTube playlist to mp3 bot/Program.cs
@@ -1,14 +1,19 @@
-﻿using Telegram.Bot;
+﻿using System.Collections.Concurrent;
+using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using YoutubeExplode;
 using YoutubeExplode.Exceptions;
+using YoutubeExplode.Playlists;
 using File = System.IO.File;
 
 namespace YouTube_playlist_to_mp3_bot;
 
 internal class Program
 {
+    private static readonly YoutubeClient Youtube = new YoutubeClient();
+    
     public static void Main()
     {
         var token = File.ReadAllText(@"..\..\..\token.txt");
@@ -18,55 +23,86 @@ internal class Program
         Console.ReadKey();
     }
 
-    private static async void MessageHandler(ITelegramBotClient client, Update update)
+    private static void MessageHandler(ITelegramBotClient client, Update update)
+    {
+        switch (update.Type)
+        {
+            case UpdateType.Message:
+                MessageReceived(client, update);
+                break;
+            case UpdateType.CallbackQuery:
+                CallbackQueryReceived(client, update);
+                break;
+            default:
+                Console.WriteLine("Unknown update type");
+                break;
+        }
+    }
+
+    private static async void MessageReceived(ITelegramBotClient client, Update update)
     {
         var reserveChatId = long.Parse(File.ReadAllText(@"..\..\..\reserveChatId.txt"));
         var chatId = update.Message?.Chat.Id ?? reserveChatId;
         var message = update.Message;
-        var youtube = new YoutubeClient();
+        var user = update.Message?.From?.Username;
 
-        if (message?.Text is null)
+        switch (message?.Text)
         {
-            await client.SendTextMessageAsync(chatId, "The message must be either a text or a link");
-            return;
+            case null:
+                await client.SendTextMessageAsync(chatId, "The message must be either a text or a link");
+                break;
+            case "/start":
+                await client.SendTextMessageAsync(chatId, "Hello!\n" +
+                                                          "I am a bot for downloading MP3 files from all videos in your playlist.\n" +
+                                                          "To start the download, please send me the link to the playlist you want to " +
+                                                          "convert to audio format.");
+                break;
+            default:
+                try
+                {
+                    var playlist = await Youtube.Playlists.GetAsync(message.Text);
+                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData("Yes", playlist.Id.ToString()),
+                            InlineKeyboardButton.WithCallbackData("No", "No")
+                        }
+                    });
+                    await client.SendTextMessageAsync(chatId, $"Are you sure you want to download audio from " + 
+                                                              $"{playlist.Count} videos from \"{playlist.Title}\" playlist?", 
+                        replyMarkup: inlineKeyboard);
+                }
+                catch (PlaylistUnavailableException)
+                {
+                    await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
+                }
+                catch (ArgumentException)
+                {
+                    await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                break;
         }
+    }
 
-        if (message.Text == "/start")
+    private static async void CallbackQueryReceived(ITelegramBotClient client, Update update)
+    {
+        var reserveChatId = long.Parse(File.ReadAllText(@"..\..\..\reserveChatId.txt"));
+        var chatId = update.CallbackQuery?.Message?.Chat.Id ?? reserveChatId;
+        var messageId = update.CallbackQuery.Message.MessageId;
+        await client.DeleteMessageAsync(chatId, messageId);
+
+        if (update.CallbackQuery.Data == "No")
         {
-            await client.SendTextMessageAsync(chatId, "Hello!\n" +
-                                                      "I am a bot for downloading MP3 files from all videos in your playlist.\n" +
-                                                      "To start the download, please send me the link to the playlist you want to " +
-                                                      "convert to audio format.");
+            await client.SendTextMessageAsync(chatId, "As you say so! You can send another playlist link to download");
         }
         else
         {
-            try
-            {
-                var playlist = await youtube.Playlists.GetAsync(message.Text);
-                var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                {
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("Yes", "Yes"),
-                        InlineKeyboardButton.WithCallbackData("No", "No")
-                    }
-                });
-                await client.SendTextMessageAsync(chatId, $"Are you sure you want to download audio from " + 
-                                                          $"{playlist.Count} videos from \"{playlist.Title}\" playlist?", 
-                    replyMarkup: inlineKeyboard);
-            }
-            catch (PlaylistUnavailableException)
-            {
-                await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
-            }
-            catch (ArgumentException)
-            {
-                await client.SendTextMessageAsync(chatId, "The link is invalid or the playlist is unavailable");
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception.Message);
-            }
+            await client.SendTextMessageAsync(chatId, "Downloading... It may take some time");
         }
     }
 }
